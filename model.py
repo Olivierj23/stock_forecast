9c86cebd5cff5865bd7c7dd972ad1146a021ad15
Olivierj23 <122296008+Olivierj23@users.noreply.github.com>
2024-07-15 13:29:17 -0400

    final edit

--- model.py
+++ model.py
@@ -4,15 +4,17 @@ import torch.nn as nn
 class ForecastModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(input_size=4, hidden_size=50, num_layers=1, batch_first=True, bidirectional=True)
-        self.linear = nn.Linear(100, 1, bias=True)
+        self.lstm = nn.LSTM(input_size=4, hidden_size=50, num_layers=2, batch_first=True, bidirectional=True, dropout=0.3)
+        self.linear2 = nn.Linear(100, 1)
+        self.linear1 = nn.Linear(100, 100)
+        self.linear3 = nn.Linear(100, 100)
         self.relu = nn.PReLU()
-        self.dropout = nn.Dropout()
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         x, _ = self.lstm(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.linear(x)
+        x = self.linear1(x)
+        x = self.linear3(x)
+        x = self.linear2(x)
 
         return x
